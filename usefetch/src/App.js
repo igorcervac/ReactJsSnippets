01054af58e03fe049547fb209ae84694dc5a9abc
Igor Cervac <igor.cervac@outlook.com>
2024-03-11 13:23:14 -0400

    Add bootstrap

--- usefetch/src/App.js
+++ usefetch/src/App.js
@@ -12,24 +12,33 @@ export default function App(){
   }
   
   return (
-      <table border="1">
-          <thead>
-          <tr>
-              <th>Craft</th>
-              <th>Name</th>
-          </tr>    
-          </thead> 
-          <tbody>           
-          {
-              data.map(p => (
-              <tr key={p.id}>
-                  <td>{p.craft}</td>
-                  <td>{p.name}</td>
-              </tr>
-              ))
-          }  
-          </tbody>         
-      </table>
+    <div className="container">
+        <div className="col-xs-12">
+            <h2 className="text-center">Astronauts</h2>
+        </div>
+        <div className="col-xs-12">
+            <table className="table table-striped table-hover" border="1">
+                <thead>
+                <tr>
+                    <th>Nr.</th>
+                    <th>Name</th>
+                    <th>Craft</th>
+                </tr>    
+                </thead> 
+                <tbody>           
+                {
+                    data.map(p => (
+                    <tr key={p.id}>
+                        <td>{p.id+1}</td>
+                        <td>{p.name}</td>
+                        <td>{p.craft}</td>
+                    </tr>
+                    ))
+                }  
+                </tbody>         
+            </table>
+      </div>
+    </div>
   );
 }           
  
