59683204f936c77e6f8379160e2ef2ba6be8f404
Igor Cervac <igor.cervac@outlook.com>
2024-03-15 11:22:44 -0400

    Remove redundant files

--- jest/src/TestComponent.test.js
+++ jest/src/TestComponent.test.js
@@ -4,5 +4,5 @@ import { render } from "@testing-library/react"
 test("Renders h2 containing Test", () => {
     const { getByText } = render(<TestComponent/>);
     const h2 = getByText(/Test/);
-    expect(h2).toHaveTextContent('Test');
+    expect(h2).toHaveTextContent("Test");
 });
